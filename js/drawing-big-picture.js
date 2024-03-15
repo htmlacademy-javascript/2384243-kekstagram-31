1da08373db0b8d2ab69c5f735939819c24700153
Nargiza <nargizyonysh@gmail.com>
2024-03-15 15:10:04 +0500

    Делает 8.15. Открывается и закрывается (часть 2)

--- js/drawing-big-picture.js
+++ js/drawing-big-picture.js
@@ -2,7 +2,15 @@ import {isEscapeKey} from './util.js';
 
 const bigPictureElement = document.querySelector('.big-picture');
 const bigPictureCloseElement = bigPictureElement.querySelector('.big-picture__cancel');
+const bigPictureImg = bigPictureElement.querySelector('.big-picture__img img');
+const bigPictureDescription = bigPictureElement.querySelector('.social__caption');
+const bigPictureLikes = bigPictureElement.querySelector('.likes-count');
+const bigPictureCommentShownCount = bigPictureElement.querySelector('.social__comment-shown-count');
+const bigPictureCommentTotalCount = bigPictureElement.querySelector('.social__comment-total-count');
+const bigPictureSocialComments = bigPictureElement.querySelector('.social__comments');
+
 const body = document.body;
+const commentsLoader = bigPictureElement.querySelector('.comments-loader'); //кнопка «Загрузить ещё»
 
 const onPictureEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -43,22 +51,43 @@ const createCommentsFragment = (comments) => {
 function openBigPicture (currentPicture) {
   bigPictureElement.classList.remove('hidden');//Для отображения окна удаляем класс hidden у элемента .big-picture
 
-  bigPictureElement.querySelector('.big-picture__img img').src = currentPicture.url;// * Адрес изображения url подставьте как src изображения внутри блока .big-picture__img.
-  bigPictureElement.querySelector('.social__caption').textContent = currentPicture.description; // * Описание фотографии description вставьте строкой в блок .social__caption.
-  bigPictureElement.querySelector('.likes-count').textContent = currentPicture.likes; // * Количество лайков likes подставьте как текстовое содержание элемента .likes-count.
-  bigPictureElement.querySelector('.social__comment-shown-count').textContent = currentPicture.comments.length; // * Количество показанных комментариев подставьте как текстовое содержание элемента .social__comment-shown-count.
-  bigPictureElement.querySelector('.social__comment-total-count').textContent = currentPicture.comments.length; // * Общее количество комментариев к фотографии comments подставьте как текстовое содержание элемента .social__comment-total-count.
-  bigPictureElement.querySelector('.social__comments').innerHTML = '';
-  bigPictureElement.querySelector('.social__comments').appendChild(createCommentsFragment(currentPicture.comments));// * Список комментариев под фотографией: комментарии должны вставляться в блок .social__comments.
+  bigPictureImg.src = currentPicture.url;// * Адрес изображения url подставьте как src изображения внутри блока .big-picture__img.
+  bigPictureDescription.textContent = currentPicture.description; // * Описание фотографии description вставьте строкой в блок .social__caption.
+  bigPictureLikes.textContent = currentPicture.likes; // * Количество лайков likes подставьте как текстовое содержание элемента .likes-count.
+  bigPictureCommentShownCount.textContent = currentPicture.comments.length; // * Количество показанных комментариев подставьте как текстовое содержание элемента .social__comment-shown-count.
+  bigPictureCommentTotalCount.textContent = currentPicture.comments.length; // * Общее количество комментариев к фотографии comments подставьте как текстовое содержание элемента .social__comment-total-count.
+
+  // function openBigPictureComments (shownComments) {
+  bigPictureSocialComments.innerHTML = '';
+  bigPictureSocialComments.appendChild(createCommentsFragment(currentPicture.comments));
+
+  commentsLoader.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    let shownComments = 5;
+    if (shownComments >= currentPicture.comments.length) {
+      commentsLoader.classList.add('hidden');
+    } else {
+      commentsLoader.classList.remove('hidden');
+      shownComments += 5;
+    }
+    bigPictureCommentShownCount.textContent = shownComments > currentPicture.comments.length ? currentPicture.comments.length : shownComments;
+  });
+
+  // commentsLoader.removeEventListener('click', (evt) => {
+  //   evt.preventDefault();
+  // });
+
+
+  //bigPictureElement.querySelector('.social__comments').appendChild(createCommentsFragment(currentPicture.comments));// * Список комментариев под фотографией: комментарии должны вставляться в блок .social__comments.
 
   document.addEventListener('keydown', onPictureEscKeydown);
 
   // После открытия окна спрячьте блоки счётчика комментариев .social__comment-count, добавив класс hidden
-  const commentsCount = bigPictureElement.querySelector('.social__comment-count');
-  commentsCount.classList.add('hidden');
+  //const commentsCount = bigPictureElement.querySelector('.social__comment-count');
+  //commentsCount.classList.add('hidden');
   // После открытия окна спрячьте блок загрузки новых комментариев .comments-loader, добавив класс hidden
-  const commentsLoader = bigPictureElement.querySelector('.comments-loader');
-  commentsLoader.classList.add('hidden');
+  //commentsLoader.classList.add('hidden');
 
   body.classList.add('modal-open');// добавьте тегу <body> класс modal-open,чтобы контейнер с фотографиями позади не прокручивался при скролле
 }
@@ -77,3 +106,10 @@ bigPictureCloseElement.addEventListener('click', () => {
 });
 
 export {openBigPicture};
+
+// кнопка «Загрузить ещё» - загрузки с сервера быть не должно не происходит. Просто показываются следующие 5 комментариев из списка.
+//Сразу после открытия изображения в полноэкранном режиме отображается не более 5 комментариев
+// следующие 5 элементов добавлялись бы по нажатию на кнопку «Загрузить ещё»/Отображение дополнительных комментариев происходит при нажатии на кнопку .comments-loader
+//При нажатии на кнопку отображается не более 5 новых комментариев
+//При изменении количества показанных комментариев число показанных комментариев в блоке .social__comment-count также изменяется
+//Если все комментарии показаны, кнопку .comments-loader следует скрыть, добавив класс hidden
