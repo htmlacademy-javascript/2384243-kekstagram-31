c6834720e05025d7e4d301e3ee4345d9fd36096f
Nargiza <nargizyonysh@gmail.com>
2024-04-10 18:03:18 +0500

    Меняет код

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
+import './img-effects.js';
 import './validate-form.js';
 import './scale-buttons.js';
-import './img-effects.js';
 import './get-data.js';
 import './send-data.js';
 import './load-photo.js';
