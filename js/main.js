679da38cd9c03d365ca88eddd1f403edcf94ae77
Nargiza <nargizyonysh@gmail.com>
2024-04-10 17:32:46 +0500

    Корректирует код

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ import './validate-form.js';
 import './scale-buttons.js';
 import './img-effects.js';
 import './get-data.js';
-import './load-photo.js';
 import './send-data.js';
+import './load-photo.js';
